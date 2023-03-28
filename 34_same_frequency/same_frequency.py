d2a11febffd72c11b1abd011ec5bbf65bcde4b22
Elle White <123752164+ElleWhiteDev@users.noreply.github.com>
2023-03-28 10:03:43 -0400

    Assignment completed

--- 34_same_frequency/same_frequency.py
+++ 34_same_frequency/same_frequency.py
@@ -10,3 +10,6 @@ def same_frequency(num1, num2):
         >>> same_frequency(1212, 2211)
         True
     """
+    first_num = str(num1)
+    second_num = str(num2)
+    return {num: first_num.count(num) for num in first_num} == {num: second_num.count(num) for num in second_num}
